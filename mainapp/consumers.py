32a7d5826b3f42654f13f14734888126893eea2e
ESME_PRATAP <pratapchoubey3@gmail.com>
2025-10-23 18:58:21 +0530

    Update consumers.py

--- mainapp/consumers.py
+++ mainapp/consumers.py
@@ -2,27 +2,18 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 
-from .models import Message  # models import safe at top-level
+from .models import Message  # safe top-level
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = "global_chat"
-
-        # Join chat group
-        await self.channel_layer.group_add(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(
-            self.room_group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
-        """Handle message received from WebSocket"""
         data = json.loads(text_data)
         username = data.get('username', 'Anonymous')
         message = data.get('message', '')
@@ -30,7 +21,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
         if message.strip():
             await self.save_message(username, message)
 
-            # Broadcast the message to all connected clients
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -41,7 +31,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
 
     async def chat_message(self, event):
-        """Send message to WebSocket"""
         await self.send(text_data=json.dumps({
             'username': event['username'],
             'message': event['message']
@@ -49,8 +38,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def save_message(self, username, message):
-        """Save message to the database safely"""
-        # Import User here to avoid AppRegistryNotReady error
+        # ⚡ Delay User import until function call
         from django.contrib.auth.models import User
         user, _ = User.objects.get_or_create(username=username)
         Message.objects.create(user=user, content=message)
