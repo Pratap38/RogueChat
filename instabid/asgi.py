4133a217cfbf4558288d982574ce2abbde8174c6
ESME_PRATAP <pratapchoubey3@gmail.com>
2025-10-23 19:07:54 +0530

    Update asgi.py

--- instabid/asgi.py
+++ instabid/asgi.py
@@ -4,10 +4,15 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from mainapp.routing import websocket_urlpatterns
 
+# Make sure settings are loaded
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'instabid.settings')
 
+# Standard HTTP ASGI app
+django_asgi_app = get_asgi_application()
+
+# Full Protocol Router for HTTP + WebSocket
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(websocket_urlpatterns)
     ),
