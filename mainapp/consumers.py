d7fd02726280c8336e3f8d8ff2d1522c737cf5e5
ESME_PRATAP <pratapchoubey3@gmail.com>
2025-10-23 19:07:01 +0530

    Update consumers.py

--- mainapp/consumers.py
+++ mainapp/consumers.py
@@ -2,25 +2,34 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 
-from .models import Message  # safe top-level
-
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_group_name = "global_chat"
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+
+        # Join chat group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
         await self.accept()
 
     async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
     async def receive(self, text_data):
+        """Handle message received from WebSocket"""
         data = json.loads(text_data)
         username = data.get('username', 'Anonymous')
         message = data.get('message', '')
 
         if message.strip():
+            # Save message in DB
             await self.save_message(username, message)
 
+            # Broadcast message to group
             await self.channel_layer.group_send(
                 self.room_group_name,
                 {
@@ -31,6 +40,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             )
 
     async def chat_message(self, event):
+        """Send message to WebSocket"""
         await self.send(text_data=json.dumps({
             'username': event['username'],
             'message': event['message']
@@ -38,7 +48,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def save_message(self, username, message):
-        # ⚡ Delay User import until function call
+        """Save message safely in DB"""
         from django.contrib.auth.models import User
+        from .models import Message
+
         user, _ = User.objects.get_or_create(username=username)
         Message.objects.create(user=user, content=message)
